0ca9c3cc33b6ed65f72e99972096538c01fecee2
williancozzi <williancozzi@gmail.com>
2020-03-06 10:46:32 -0300

    ajuste nas validacoes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-    //adicionando listener no botao enviar para checar a situação do form nome
-    var button = document.getElementById("enviar");
+//adicionando listener no botao enviar para checar a situação do form nome
+var button = document.getElementById("enviar");
     button.addEventListener("click", function(e) {
-        var nome = document.getElementById('nome').value.split(' ');
-        if(nome.length > 1) {
-            alert('Enviado com sucesso.');
-            document.getElementById('formCadastro').submit();
-        } else {
+        var nome = document.getElementById('nome').value.split(' ', 2);
+        if((nome.length < 2) || (nome[0] == '') || (nome[1] == '')) {
             alert('O nome deve possuir pelo menos um sobrenome.');
+        } else {
+            document.getElementById('formCadastro').submit();
+            alert('Enviado com sucesso.');
         }
     }, false);
 
+//funçao que valida o telefone para somente numero num input text
 function aceitarSomenteNumeros(num) {
+    
     var er = /[^0-9]/;
     er.lastIndex = 0;
     var campo = num;
@@ -20,14 +22,16 @@ function aceitarSomenteNumeros(num) {
     }
 }
 
+
+//mostrar as midias sociais caso possui rede social
 function mostrarMidiasSociais(el) {
+    
     var display = document.getElementById(el).style.display;
     if (display == "none" )
-      document.getElementById(el).style.display = 'block';
-    else
-      document.getElementById(el).style.display = 'none';
-  }
+        document.getElementById(el).style.display = 'block';
+}
 
-  function confirmar(){
-
-  }
+//esconder as midias sociais 
+  function esconderMidiasSociais(el){
+    document.getElementById(el).style.display = 'none';
+}
