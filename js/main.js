64dc5fc0a8f53c6e29f44d95bc5e7b37bbce41bb
williancozzi <williancozzi@gmail.com>
2020-03-20 23:12:02 -0300

    removendo o txt e ajustando o js

--- js/main.js
+++ js/main.js
@@ -1,99 +1,109 @@
-var dadosParaEnviar = null;
-
-//adicionando listener no botao enviar para checar a situação do form nome, e caso validado, preparar para envio o json no endpoint
 var button = document.getElementById("enviar");
-button.addEventListener("click", function(e) {
-    var nome = document.getElementById('nome').value.split(' ', 2);
-    if ((nome.length < 2) || (nome[0] == '') || (nome[1] == '')) {
-        alert('O nome deve possuir pelo menos um sobrenome.');
-    } else {
-        alert('Enviado com sucesso.');
-        getDados();
-        document.getElementById('formCadastro');
-        (async() => {
-            const rawResponse = await fetch("http://localhost:8080", {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(dadosParaEnviar)
-            });
-            const content = await rawResponse.json();
+var dadosParaEnviar = null;
+var valido = false;
 
-            console.log(content);
-        })();
-        //limpar os forms pro proximo uso
-        document.forms[0].reset();
-        zerarCheckboxes();
-    }
-}, false);
+//Adicionando listener no botao enviar para checar a situação do form nome, e caso validado, preparar para envio o json no endpoint
+button.addEventListener(
+    "click",
+    function(e) {
+        var nome = document.getElementById("nome").value.split(" ", 2);
+        if (nome.length < 2 || nome[0] == "" || nome[1] == "") {
+            alert("O nome deve possuir pelo menos um sobrenome.");
+            valido = false;
+        } else {
+            valido = true;
+            alert("Enviado com sucesso.");
+            getDados();
+            document.getElementById("formCadastro");
+            (async() => {
+                const rawResponse = await fetch("http://localhost:8080", {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(dadosParaEnviar)
+                });
+                const content = await rawResponse.json();
+
+                console.log(content);
+            })();
+            //limpar os forms pro proximo uso
+            document.forms[0].reset();
+            zerarCheckboxes();
+        }
+    },
+    false
+);
 
 //funçao que valida o telefone para somente numero num input text
 function aceitarSomenteNumeros(num) {
-
     var er = /[^0-9]/;
     er.lastIndex = 0;
     var campo = num;
     if (er.test(campo.value)) {
-        alert('Somente números podem ser aceitos em telefone');
+        alert("Somente números podem ser aceitos em telefone");
         campo.value = "";
     }
 }
 
 //mostrar as midias sociais caso possuir rede social
 function mostrarMidiasSociais(el) {
-
     var display = document.getElementById(el).style.display;
-    if (display == "none")
-        document.getElementById(el).style.display = 'block';
+    if (display == "none") document.getElementById(el).style.display = "flex";
 }
 
-//esconder as midias sociais 
+//esconder as midias sociais
 function esconderMidiasSociais(el) {
     zerarCheckboxes();
-    document.getElementById(el).style.display = 'none';
+    document.getElementById(el).style.display = "none";
 }
 
 function zerarCheckboxes() {
     midias = [];
-    document.getElementById('checkfacebook').checked = false;
-    document.getElementById('checklinkedin').checked = false;
-    document.getElementById('checkinstagram').checked = false;
+    document.getElementById("checkfacebook").checked = false;
+    document.getElementById("checklinkedin").checked = false;
+    document.getElementById("checkinstagram").checked = false;
 }
 
-//passando pra json 
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.getElementById('enviar').addEventListener('click', getDados);
+//passando pra json
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("enviar").addEventListener("click", getDados);
 });
 
 let midias = [];
 
 function getDados(ev) {
-
     let dado = {
-        nome: document.getElementById('nome').value,
-        telefone: document.getElementById('ddd').value + '-' + document.getElementById('fone').value,
-        comoConheceu: document.getElementById('comoConheceu').value,
+        nome: document.getElementById("nome").value,
+        telefone: document.getElementById("ddd").value +
+            "-" +
+            document.getElementById("fone").value,
+        comoConheceu: document.getElementById("comoConheceu").value,
         midias: midias.length != 0 ? midias : midias
-    }
+    };
 
-    if (document.getElementById('temMidiaSocial').value == 's') { //checar se o radio midia social está selecionado
+    if (document.getElementById("temMidiaSocial").value == "s") {
 
-        if (document.getElementById('checkfacebook').checked == true) {
-            midias.push('Facebook');
+        //checar se o radio midia social está selecionado
+        if (
+            document.getElementById("checkfacebook").checked == true &&
+            valido === true
+        ) {
+            midias.push("Facebook");
         }
-        if (document.getElementById('checklinkedin').checked == true) {
-            midias.push('Linkedin');
+        if (
+            document.getElementById("checklinkedin").checked == true &&
+            valido === true
+        ) {
+            midias.push("Linkedin");
         }
-        if (document.getElementById('checkinstagram').checked == true) {
-            midias.push('Instagram');
-
+        if (
+            document.getElementById("checkinstagram").checked == true &&
+            valido === true
+        ) {
+            midias.push("Instagram");
         }
     }
     dadosParaEnviar = dado;
-
-
-
 }
